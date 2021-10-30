8540439c373f4470909386a5c3242457790a2225
Muhammad Faturahman Bancin <mufatraba@gmail.com>
2021-10-30 08:24:06 +0700

    add Movement to Circle (Problem 2) 🔧

---
+++ Assets/Scripts/BallMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallMovement : MonoBehaviour
+{
+    Rigidbody2D rigidbody2D;
+
+
+    public float speed = 10f;
+
+    void Start()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        float randomDirection = Random.Range(0, 2);
+        rigidbody2D.velocity = new Vector2(randomDirection * speed, randomDirection * speed);
+    }
+}
