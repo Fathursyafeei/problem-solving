fce78ac62c5fd676477478447da5b129376cccd8
Muhammad Faturahman Bancin <mufatraba@gmail.com>
2021-10-30 09:44:51 +0700

    Add Bounce Material and FIx Movement (Problem 3) 🛠

--- Assets/Scripts/BallMovement.cs
+++ Assets/Scripts/BallMovement.cs
@@ -8,15 +8,32 @@ public class BallMovement : MonoBehaviour
 
 
     public float speed = 10f;
+    public float xForce;
+    public float yForce;
 
     void Start()
     {
         rigidbody2D = GetComponent<Rigidbody2D>();
     }
 
+    void PushBall()
+    {
+        float arahRandom = Random.Range(0, 2);
+        if (arahRandom < 1.0f)
+        {
+            rigidbody2D.AddForce(new Vector2(-xForce, yForce));
+        }
+        else
+        {
+            rigidbody2D.AddForce(new Vector2(xForce, -yForce));
+        }
+        Debug.Log(arahRandom);
+    }
+
     private void FixedUpdate()
     {
-        float randomDirection = Random.Range(0, 2);
-        rigidbody2D.velocity = new Vector2(randomDirection * speed, randomDirection * speed);
+        PushBall();
+        //float randomDirection = Random.Range(0, 2);
+        //rigidbody2D.velocity = new Vector2(randomDirection * speed, randomDirection * speed);
     }
 }
